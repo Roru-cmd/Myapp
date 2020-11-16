ce1cb3bd6bdb12cccebfc6fbefb6ea9dcaf61950
Roru-cmd <0859@ro.ru>
2020-11-16 14:10:03 +0900

    Added User Last updated Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,4 @@ class Task < ApplicationRecord
   validates :t_title, presence: true,
             length: { maximum: 255 } 
 
-  def self.recent(max = 1)
-    limit(max).order(created_at: :desc)
-    end
 end
