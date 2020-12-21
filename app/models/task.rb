c0f1eac098b9c76c52a2d7959a4314c57b3ae918
Roru-cmd <0859@ro.ru>
2020-12-21 13:10:08 +0900

    Making task_id work

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :users, optional: true
+  default_scope -> { order(created_at: :desc) }
   validates :t_title, presence: true,
             length: { maximum: 255 } 
   has_many :tickets, dependent: :destroy
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :user
-  belongs_to :task, optional: true
+  belongs_to :task, optional: true 
+  #validates :task_id, presence: true
+  default_scope -> { order(created_at: :desc) }
 end
