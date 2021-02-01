010802a042e8ac809ebe364b227a3581f1ed5d42
Roru-cmd <0859@ro.ru>
2021-02-01 16:53:29 +0900

    Select for Tickets failed

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -21,17 +21,17 @@ class TicketsController < ApplicationController
   end
 
   # GET /tickets/1/edit
-  def edit
+  def edit   
   end
 
   # POST /tickets
   # POST /tickets.json
   def create
-    @ticket = Ticket.new(ticket_params)
+    #@ticket = Ticket.new(ticket_params)
     #@user = User.find(user_id: params[:user_id]) 
-    @user = User.find(params[:user_id])    
+    @user = User.find(params[:user_id])    #work   
     @ticket = @user.tickets.build(ticket_params)
-    #@user = User.find(params[:user_id]) # Error "User Must Exist" if has_many :tickets, through: :tasks    
+    # @user = User.find(params[:user_id]) # Error "User Must Exist" if has_many :tickets, through: :tasks    
     # @user = Task.find_by(user_id:params[:task_id]) # Do not work
     # @user = Task.find_by(user_id:params[:user_id]) # Error
     # @ticket = @user.tasks.create(ticket_params) # Error    
