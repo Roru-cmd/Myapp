e5b7c8ceeff7e32b79f9e6ca413efabac179adb2
Roru-cmd <0859@ro.ru>
2021-04-13 17:23:26 +0900

    User show changed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   get 'users/index'
   get "/" => "users#home"
+  get "tasks/new" => "tasks#new"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
