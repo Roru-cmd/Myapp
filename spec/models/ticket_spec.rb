edac4f673ffb39e6a996bf9c4e09867d109295d3
Roru-cmd <0859@ro.ru>
2021-05-24 10:13:04 +0900

    Valid attributes test added to Ticket model

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Ticket, :type => :model do
 
-    it "is valid with valid attributes"    
+  it "is valid with valid attributes" do
+    @ticket1 = create(:ticket)
+    expect(ticket1).to be_valid
+end   
     it "is not valid without a description"
     it "is not valid without a start_date"
     it "is not valid without a end_date"
