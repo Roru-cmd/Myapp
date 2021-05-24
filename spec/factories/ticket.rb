fe67f94c20d89c482e03d8bc7e836d64a50caf61
Roru-cmd <0859@ro.ru>
2021-05-24 11:10:42 +0900

    Added user_id check to ticket test

--- spec/factories/ticket.rb
+++ spec/factories/ticket.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
 
   factory :ticket do
       sequence(:ticket_d)      { |n| "Ticket#{n}" }
-      sequence(:ticket_start)  { Time.now }
-      sequence(:ticket_end)    { Time.now }
+      ticket_start             { Time.now }
+      ticket_end               { Time.now }
+      sequence(:user_id)       { |n| "user_id #{n}" }
       created_at               { Time.now }
       updated_at               { Time.now + 1.day }
   end
