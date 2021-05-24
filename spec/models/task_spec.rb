d58a120726f3e584a5e3ad0ae00c29e376083911
Roru-cmd <0859@ro.ru>
2021-05-24 17:06:57 +0900

    Testing Task belongs_to User

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
 
-    before(:all) do
-        @task1 = create(:task)        
-    end
+    #before(:all) do
+        #@task1 = create(:task)        
+    #end
 
     it "is valid with valid attributes" do
         @task1 = create(:task)
@@ -16,7 +16,20 @@ RSpec.describe Task, :type => :model do
         expect(task2).not_to be_valid
     end
 
-    it "is not valid without a user_id" do
+    it "has title not longer then 255" do
+        task2 = build(:task)
+        expect(task2.validates_length_of(:t_title, :maximum => 255))       
     end
-    
+
+    it "has many tickets" do
+        should respond_to(:tickets)
+    end
+   
+    it "belongs to user" do
+        task2 = build(:task)
+        #expect(task2.reflect_on_association(:user).macro).to be (:belongs_to)
+        #expect(task2.reflect_on_association(:user).macro).to eq :belongs_to
+    end
+
+    #it { should belong_to(:user) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,14 +11,18 @@ RSpec.describe User, :type => :model do
         expect(@user1).to be_valid
     end
 
+    it "has many tasks" do
+        should respond_to(:tasks)
+    end
+
     it "has uniqe username" do          
         user2 = build(:user, :number)
         expect(user2).to be_valid
     end
 
-    it "is not valid without a time" do
-        user2 = build(:user, time: nil)
-        expect(user2).not_to be_valid
-    end
+    #it "is not valid without a time" do
+        #user2 = build(:user, time: nil)
+        #expect(user2).not_to be_valid
+    #end
 
 end
